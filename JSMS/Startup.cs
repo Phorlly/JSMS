c2191c0abac3dcd5df8690ab9aca2bad2c537ada
lannphorll.698896@gmail.com <Mw8800$@>
2023-12-02 07:12:46 +0700

    Add project files.

---
+++ JSMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JSMS.Startup))]
+namespace JSMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
